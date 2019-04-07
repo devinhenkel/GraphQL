07b4a8fc34050b9f584316b3d73d9560d2429f28
Devin Henkel <devin@devinhenkel.com>
2019-04-06 23:30:42 -0500

    plennty of changes

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,6 +1,50 @@
 //import GraphQLServer
 import { GraphQLServer } from 'graphql-yoga'
 
+// data
+let POSTS = [
+    {
+        id: '111',
+        title: 'The Sun Also Rises',
+        body: 'No it don\'t...',
+        published: true
+    },
+    {
+        id: '222',
+        title: 'The Moon Also Sinks',
+        body: 'Oh, yes it do.',
+        published: true
+    },
+    {
+        id: '333',
+        title: 'Pluto is a dog planet',
+        body: 'Bark, bark.',
+        published: false
+    }
+]
+
+let PEOPLE = [
+    {
+        id: '444',
+        firstname: 'Devin',
+        lastname: 'Henkel-Legare',
+        email: 'devin@devinhenkel.com'
+    },
+    {
+        id: '555',
+        firstname: 'Laura',
+        lastname: 'Legare',
+        email: 'lauralegare@live.com'
+    },
+    {
+        id: '666',
+        firstname: 'Crusty',
+        lastname: 'McNugget',
+        email: 'crusty@mcnugget.com'
+    }
+]
+
+
 // type types
 // Scalar types: String, Boolean, Int, Float, ID
 // Collections: Object, Array
@@ -8,16 +52,27 @@ import { GraphQLServer } from 'graphql-yoga'
 // type definitions (schema)
 const typeDefs = `
     type Query {
+        me: Person!
+        post(id: ID!): Post!
+        posts: [Post!]
+        people: [Person]
+        person(id: ID!): Person!
+        grades: [Int!]!
+        add(numbers: [Float!]!): Float!
+    }
+
+    type Person {
         id: ID!
-        name: String!
-        age: Int!
-        employed: Boolean!
-        gpa(num: Int): Float
+        firstname: String!
+        lastname: String!
+        email: String!
+    }
+
+    type Post {
+        id: ID!
         title: String!
-        price: Float!
-        rating: Int!
-        released: Int!
-        inStock: Boolean
+        body: String!
+        published: Boolean!
     }
 `
 
@@ -25,33 +80,32 @@ const typeDefs = `
 // resolvers (functions)
 const resolvers = {
     Query: {
-        id() {
-            return "abc123"
+        me() {
+            return PEOPLE.find((person)=> person.id === "444")
         },
-        name() {
-            return "D-Dogg Diggity Dogg"
+        // passed props (parent, args, context[state], info)
+        post: (parent, args, context, info) => {
+            return POSTS.find((post)=> post.id === args.id)
         },
-        age() {
-            return 54
+        posts: () => {
+            return POSTS
         },
-        employed() {
-            return true
+        people: () => {
+            return PEOPLE
         },
-        gpa: (_, {num}) => num/100,
-        title() {
-            return "Return of the Jed Eye"
+        person: (parent, args, context, info) => {
+            return PEOPLE.find((person)=> person.id === args.id)
         },
-        price() {
-            return 19.99
+        grades() {
+            return [10,20,30,40,50,60,70,80,90]
         },
-        inStock() {
-            return true
-        },
-        rating() {
-            return 5
-        },
-        released() {
-            return 1999
+        add: (parent, args, context, info) => {
+            if (args.numbers.length ===0) {
+                return 0
+            } else {
+                return args.numbers.reduce((sum, current) => sum + current)
+                
+            }
         }
     }
 }
@@ -63,5 +117,5 @@ const server = new GraphQLServer({
 })
 
 server.start(() => {
-    console.log("GraphQLServer is up...")
+    console.log("GraphQLServer is up on port 4000 ...")
 })
